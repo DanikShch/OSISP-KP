69582f90460f72ee6fa0fbdd8b83743aeca89545
DanikShch <147268869+DanikShch@users.noreply.github.com>
2024-04-28 19:04:53 +0300

    threads

--- file_trancfer.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILESHARING_FILE_TRANCFER_H
-#define FILESHARING_FILE_TRANCFER_H
-
-void send_file(const char *path, int fd);
-void recieve_file(const char *path, int fd);
-
-#endif //FILESHARING_FILE_TRANCFER_H
